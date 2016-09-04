957fa2682902bdd1c1f9818d12a06f07c0c2d6ac
Gary Pang <gary.pang@gmail.com>
2016-09-04 03:56:41 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Sample Mocha and Chai test #
+_by Gary Pang, "CodeWritingCow"_
+
+This is a sample Mocha and Chai test. From the SitePoint [tutorial](https://www.sitepoint.com/unit-test-javascript-mocha-chai), "Unit Test Your JavaScript Using Mocha and Chai."
---
+++ js/className.js
@@ -0,0 +1,23 @@
+/*
+
+function addClass(el, newClass) {
+	if (el.className.indexOf(newClass) === -1) {
+		el.className += newClass;
+	}
+}
+ */
+
+ // Node version
+ module.exports = {
+ 	addClass: function(el, newClass) {
+ 		if (el.className.indexOf(newClass) !== -1) {
+ 			return;
+ 		}
+
+ 		if (el.className !== '') {
+ 			newClass = ' ' + newClass;
+ 		}
+
+ 		el.className += newClass;
+ 	}
+ };
